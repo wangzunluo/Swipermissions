edfbbcfe13d6a6867dbe57a1b590e531c6328aab
wangzunluo <wangarwin13@gmail.com>
2019-03-19 14:17:49 -0500

    working on styling components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Table from './Table';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. Change when you save.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="TopContainer">
+          <div className="Title">Machine Availability</div>
+        </div>
+        <Table></Table>
       </div>
     );
   }
