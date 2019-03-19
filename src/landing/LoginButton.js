6ea909c83e92c205ac20634edc004044b20dbc2b
wangzunluo <wangarwin13@gmail.com>
2019-03-19 17:33:00 -0500

    login button added

---
+++ src/landing/LoginButton.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './LoginButton.css'
+
+class LoginButton extends Component
+{
+  render()
+  {
+    return (
+      <button>
+        LOGIN
+      </button>
+    )
+  }
+}
+
+export default LoginButton
