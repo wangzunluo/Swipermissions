685d5bde60dea416ece4110b919646516baa3173
MatthewRamsey15085 <31446909+MatthewRamsey15085@users.noreply.github.com>
2019-03-24 19:12:42 -0500

    add login popup and ID entry

--- src/landing/App.js
+++ src/landing/App.js
@@ -3,8 +3,48 @@ import Table from './Table';
 import LoginButton from './LoginButton';
 import './App.css';
 
-class App extends Component {
+class Login extends React.Component {
   render() {
+    return (
+      <div className='Login'>
+        <div className='Login_inner'>
+          <div className='Close_bar'>
+            <button className ='closer' onClick={this.props.closePopup}>X</button>
+          </div>
+          <div className ='IDEntry'>
+            <form>
+              <label>
+                User ID:
+                <input type="text" name="name" />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+
+        </div>
+ 
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      showPopup: false
+    };
+  }
+  
+  togglePopup() {
+    this.setState({
+      showPopup: !this.state.showPopup
+    });
+  }
+
+  render() {
+
     return (
       <div className= "Wrapper">
         <div className="TopContainer">
@@ -14,7 +54,15 @@ class App extends Component {
             
           </div>
           <div className="TopRight">
-            <LoginButton></LoginButton>
+            {/*<LoginButton></LoginButton>*/}
+            <button variant ='outline-danger' className ='logButton' onClick={this.togglePopup.bind(this)}>LOGIN</button>
+            {this.state.showPopup ? 
+              <Login
+                
+                closePopup={this.togglePopup.bind(this)}
+              />
+              : null
+        }
           </div>
         </div>
         <div className="BottomContainer">
