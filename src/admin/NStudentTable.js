8ce5d4f82ecf208280ce88fdfe5688bd2363baaa
LMR4547 <mengrui.luo@gmail.com>
2019-04-25 13:46:59 -0500

    Adding checkbox for last 5 columns

--- src/admin/NStudentTable.js
+++ src/admin/NStudentTable.js
@@ -1,4 +1,4 @@
-import BootstrapTable from 'react-bootstrap-table-next';
+import BootstrapTable, {TableHeaderColumn} from 'react-bootstrap-table-next';
 import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
 import './react-bootstrap-table2.min.css' ;
 import React, { Component } from 'react';
@@ -7,12 +7,11 @@ import { Type } from 'react-bootstrap-table2-editor';
 //import './bootstrap.css'
 
 
-
 const { SearchBar } = Search;
 const columns = [{
   //style: "color: white;",
   dataField: 'name',
-  text: 'Name'
+  text: 'Name',
 }, {
   dataField: 'email',
   text: 'Contact'
@@ -20,49 +19,64 @@ const columns = [{
 
   dataField: 'm1',
   text: 'Mill',
-  type: Type.CHECKBOX,
-  value: 'Y:N',
-  editor:{
-    type: Type.CHECKBOX,
-    value: 'Y:N'
-  }
+  lookup: {1: 'yes', 2: 'no'},
+  formatter: cellFormatter
 },
 {
 
     dataField: 'm2',
     text: 'Lathe',
-    lookup: {1: 'yes', 2: 'no'}
+    lookup: {1: 'yes', 2: 'no'},
+    formatter: cellFormatter
 },
 {
 
     dataField: 'm3',
     text: 'CNC Mill',
-    lookup: {1: 'yes', 2: 'no'}
+    lookup: {1: 'yes', 2: 'no'},
+    formatter: cellFormatter
 },
 {
 
     dataField: 'm4',
     text: 'CNC Router',
-    lookup: {1: 'yes', 2: 'no'}
+    lookup: {1: 'yes', 2: 'no'},
+    formatter: cellFormatter
 },
 {
 
     dataField: 'm5',
     text: 'CNC Plasma',
-    lookup: {1: 'yes', 2: 'no'}
+    lookup: {1: 'yes', 2: 'no'},
+    formatter: cellFormatter
 }];
+
+function cellFormatter(cell, row) {
+  if (cell == 2) {
+    return (
+      <input
+            type="checkbox"
+            checked
+            />
+    );
+  }
+
+  return (
+    <input type="checkbox"/>
+  );
+}
+
 class NStudentTable extends Component{
     constructor(){
         super();
-
         this.state = 
         {
             data:JSON.parse(localStorage.getItem('data')) ||
             [
-                { name: 'Matthew Ramsey', email: "Matthew.M.Ramsey-1@ou.edu", m1: 'Y', m2: 2, m3: 1, m4: 2, m5: 2 },
+                { name: 'Matthew Ramsey', email: "Matthew.M.Ramsey-1@ou.edu", m1: 1, m2: 2, m3: 1, m4: 2, m5: 2 },
                 { name: 'Arwin', email: "arwin@ou.edu", m1: 1, m2: 2, m3: 1, m4: 2, m5: 2 },
                 { name: 'Jake', email: "baerj@ou.edu", m1: 2, m2: 2, m3: 1, m4: 2, m5: 2 },
-                { name: 'Matthew Ramsey', email: "Matthew.M.Ramsey-1@ou.edu", m1: 'Y', m2: 2, m3: 1, m4: 2, m5: 2 },
+                { name: 'Matthew Ramsey', email: "Matthew.M.Ramsey-1@ou.edu", m1: 1, m2: 2, m3: 1, m4: 2, m5: 2 },
                 { name: 'Arwin', email: "arwin@ou.edu", m1: 1, m2: 2, m3: 1, m4: 2, m5: 2 },
 
             ]
@@ -119,7 +133,7 @@ class NStudentTable extends Component{
         return(
             <ToolkitProvider className= "myTable"
                 bootstrap4 = {true}
-                keyField="id"
+                keyField="Name"
                 data= {this.state.data}
                 
                 columns={ columns }
@@ -135,7 +149,6 @@ class NStudentTable extends Component{
 
                         <BootstrapTable 
                          bootstrap4 = {true}
-
                         { ...props.baseProps }
                         />
                         <button onClick= {this.addRow.bind(this)}>Add Student</button>
@@ -149,5 +162,5 @@ class NStudentTable extends Component{
     }
 }
 
-export default NStudentTable
+export default NStudentTable;
 
