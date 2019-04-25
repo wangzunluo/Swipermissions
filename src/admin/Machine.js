90cb1a1e068bbe9da400dad4802d2585598acd80
MatthewRamsey15085 <31446909+MatthewRamsey15085@users.noreply.github.com>
2019-04-24 21:25:42 -0500

    pass state between class test

--- src/admin/Machine.js
+++ src/admin/Machine.js
@@ -8,6 +8,9 @@ import AddIcon from '@material-ui/icons/Add';
 import InfoIcon from '@material-ui/icons/Info'
 
 
+
+
+
 class Machine extends Component {
   constructor(props) {
     super(props);
@@ -32,11 +35,23 @@ class Machine extends Component {
       var oldCheck = JSON.parse(localStorage.getItem('checkedOut' + this.props.id));
     }
 
+    if (JSON.parse(localStorage.getItem('user' + this.props.id) == undefined))
+    {
+      //var color = JSON.parse(localStorage.getItem('green' + this.props.id));
+      var oldUser = "";
+    }
+    else
+    {
+      var oldUser = JSON.parse(localStorage.getItem('user' + this.props.id));
+    }
+
     this.state = {
       showPopup: false, 
       hasLoggedIn: false,
       logs: oldlog,
       checkedOut: oldCheck,
+      user: oldUser,
+      waiting: true,
     };
 
     this.togglePopup = this.togglePopup.bind(this);
@@ -59,6 +74,13 @@ class Machine extends Component {
   {
     if (!this.state.checkedOut)
     {
+      this.togglePopup();
+      this.setState(
+        {
+          waiting: true
+        }
+      );
+
       //try to prompt user
     }
     this.setState(
@@ -76,10 +98,20 @@ class Machine extends Component {
     var time = today.getHours() + ":" + today.getMinutes(); 
 
     var pastLog = this.state.logs;
+    while (this.state.waiting)
+    {
+      break;
+    }
     if (!this.state.checkedOut)
     {
-      var newLog = pastLog + date + " " + time + "  --  ";
-
+      var newLog = this.state.user + ": " + pastLog + date + " " + time + "  --  ";
+      //localStorage.removeItem("user" + this.props.id);
+      /*this.setState(
+        {
+          user: ""
+        }
+      );
+      */
     }
     else
     {
@@ -104,6 +136,13 @@ class Machine extends Component {
         <td className= "MName">{this.props.name}</td>
         <td className = "KeyBox">
         <ChangingButton id= {this.props.id} triggerParentUpdate= {this.checkOut.bind(this)}></ChangingButton>
+        {this.state.showPopup ? 
+                <Checkout id = {this.props.id}
+                    
+                    closePopup={this.togglePopup.bind(this)}
+                />
+                : null
+                }
         </td>
         <td className = "InfoBox">
         <div className = 'Fourth'>        
@@ -125,4 +164,82 @@ class Machine extends Component {
   }
 }
 
+class Checkout extends Machine {
+  constructor(props) {
+    super(props);
+    this.state = 
+    {
+      user: '',
+      password: '',
+      id: this.props.id,
+    };
+
+    this.handleChangeUser = this.handleChangeUser.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+
+
+  handleChangeUser(event) {
+    this.setState({user: event.target.value});
+  }
+
+  handleChangePassword(event) {
+    this.setState({password: event.target.value});
+  }
+
+  handleSubmit(event) {
+    if (this.state.user !== '')
+    {
+      
+      alert('A name was submitted: ' + this.state.user + this.state.id);
+      super.setState(
+        {
+          user: this.state.user,
+        },() => 
+        {
+          localStorage.setItem("user" + this.state.id, JSON.stringify(this.state.user))
+          
+        }
+      );
+      super.setState({
+        waiting: false,
+      });
+      
+    }
+    else if (this.state.user === '')
+    {
+      alert("Please enter a user");
+      event.preventDefault();
+    }
+  }
+
+  render() {
+    return (
+      <div className='Login'>
+        <div className='Login_inner'>
+          <div className='Close_bar'>
+            <button className ='closer' onClick={this.props.closePopup}>X</button>
+          </div>
+          
+          <div className ='SignIn'>
+            <form className='SignForm' onSubmit={this.handleSubmit}>
+            Check out to?
+              <label className='UserBar'>
+                
+                <input type="text" value={this.state.user} onChange={this.handleChangeUser} />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+
+        </div>
+ 
+      </div>
+    );
+  }
+}
 export default Machine
