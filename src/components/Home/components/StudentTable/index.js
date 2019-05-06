b65146f1747c29273df6a1f413f86f311c0a5360
Zunluo Wang <wangarwin13@gmail.com>
2019-05-06 03:50:58 -0500

    checkboxes now update data in the database

--- src/components/Home/components/StudentTable/index.js
+++ src/components/Home/components/StudentTable/index.js
@@ -1,350 +1,267 @@
 import BootstrapTable from 'react-bootstrap-table-next';
-import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
-import './react-bootstrap-table2.min.css' ;
-import React, { Component } from 'react';
-import { ExcelButton } from './excel';
-import { compose } from 'recompose';
-import { withRouter } from 'react-router-dom';
+import ToolkitProvider, {Search} from 'react-bootstrap-table2-toolkit';
+import './react-bootstrap-table2.min.css';
+import React, {Component} from 'react';
+import {ExcelButton} from './excel';
+import {compose} from 'recompose';
+import {withRouter} from 'react-router-dom';
 
-import { withFirebase } from '../../../Firebase';
+import {withFirebase} from '../../../Firebase';
 
 //import './bootstrap.css'
 
+const {SearchBar} = Search;
 
-const { SearchBar } = Search;
-const columns = [{
-  dataField: 'name',
-  text: 'Name',
-}, {
-  dataField: 'email',
-  text: 'Contact'
-}, {
-  text: 'Mill',
-  formatter: cellFormatter1
-},
-{
-    text: 'Lathe',
-    formatter: cellFormatter2
-},
-{
-    text: 'CNC Mill',
-    formatter: cellFormatter3
-},
-{
-    text: 'CNC Router',
-    formatter: cellFormatter4
-},
-{
-    text: 'CNC Plasma',
-    formatter: cellFormatter5
-}];
-
-function saveCheck(spot, toCheck)
-{
+function saveCheck(spot, toCheck) {
     //var spot = row + "-" + column;
     localStorage.setItem("check" + spot, JSON.stringify(!toCheck));
     //alert("reached" + spot);
 }
 
-function cellFormatter1(cell,row, rowIndex) {
-    //alert(rowIndex + "-" + 1);
-    var spot = rowIndex + "-" + 1;
-    var toCheck = false;
-
-    if (!(JSON.parse(localStorage.getItem("check" + spot)) == undefined))
-    {
-        toCheck = JSON.parse(localStorage.getItem("check" + spot));
-        //alert(toCheck);
-    }
-
-    if (toCheck)
-    {
-        return (
-            <input
-                type="checkbox"
-                checked
-                />
-        );
-    }
-    
-
-  return (
-    <input type="checkbox" onClick= {() => saveCheck(spot, toCheck)}/>
-  );
-}
-
-function cellFormatter2(cell, row, rowIndex) {
-  
-    var spot = rowIndex + "-" + 2;
-    var toCheck = false;
-
-    if (!(JSON.parse(localStorage.getItem("check" + spot)) == undefined))
-    {
-        toCheck = JSON.parse(localStorage.getItem("check" + spot));
-        //alert(toCheck);
-    }
-
-    if (toCheck)
-    {
-        return (
-            <input
-                type="checkbox"
-                checked
-                />
-        );
-    }
-    
-
-  return (
-    <input type="checkbox" onClick= {() => saveCheck(spot, toCheck)}/>
-  );
-}
-
-function cellFormatter3(cell, row, rowIndex) {
-  
-    var spot = rowIndex + "-" + 3;
-    var toCheck = false;
-
-    if (!(JSON.parse(localStorage.getItem("check" + spot)) == undefined))
-    {
-        toCheck = JSON.parse(localStorage.getItem("check" + spot));
-        //alert(toCheck);
-    }
-
-    if (toCheck)
-    {
-        return (
-            <input
-                type="checkbox"
-                checked
-                />
-        );
-    }
-    
-
-  return (
-    <input type="checkbox" onClick= {() => saveCheck(spot, toCheck)}/>
-  );
-}
-
-function cellFormatter4(cell, row, rowIndex) {
-  
-    var spot = rowIndex + "-" + 4;
-    var toCheck = false;
-
-    if (!(JSON.parse(localStorage.getItem("check" + spot)) == undefined))
-    {
-        toCheck = JSON.parse(localStorage.getItem("check" + spot));
-        //alert(toCheck);
-    }
-
-    if (toCheck)
-    {
-        return (
-            <input
-                type="checkbox"
-                checked
-                />
-        );
-    }
-    
-
-  return (
-    <input type="checkbox" onClick= {() => saveCheck(spot, toCheck)}/>
-  );
-}
-
-function cellFormatter5(cell, row, rowIndex) {
-  
-    var spot = rowIndex + "-" + 5;
-    var toCheck = false;
-
-    if (!(JSON.parse(localStorage.getItem("check" + spot)) == undefined))
-    {
-        toCheck = JSON.parse(localStorage.getItem("check" + spot));
-        //alert(toCheck);
-    }
-
-    if (toCheck)
-    {
-        return (
-            <input
-                type="checkbox"
-                checked
-                />
-        );
-    }
-    
-
-  return (
-    <input type="checkbox" onClick= {() => saveCheck(spot, toCheck)}/>
-  );
-}
-
-
-class NStudentTable extends Component{
-    constructor(props){
+class NStudentTable extends Component {
+    constructor(props) {
         super(props);
+
+        this.columns = [
+            {
+                dataField: 'FirstName',
+                text: 'First Name'
+            }, {
+                dataField: 'LastName',
+                text: 'Last Name'
+            }, {
+                dataField: 'Email',
+                text: 'Contact'
+            }, {
+                text: 'Mill',
+                formatter: this.cellFormatter1
+            }, {
+                text: 'Lathe',
+                formatter: this.cellFormatter2
+            }, {
+                text: 'CNC Mill',
+                formatter: this.cellFormatter3
+            }, {
+                text: 'CNC Router',
+                formatter: this.cellFormatter4
+            }, {
+                text: 'CNC Plasma',
+                formatter: this.cellFormatter5
+            }
+        ];
 
         //localStorage.clear();
         var oldData = [];
 
-        if (!(JSON.parse(localStorage.getItem('data')) == undefined))
-        {
+        if (!(JSON.parse(localStorage.getItem('data')) == undefined)) {
             console.log(oldData)
             oldData = JSON.parse(localStorage.getItem('data'));
         }
 
-        this.state = 
-        {
+        this.state = {
             data: oldData,
-            showPopup: false,
+            showPopup: false
         };
 
-        this.togglePopup = this.togglePopup.bind(this);
+        this.togglePopup = this
+            .togglePopup
+            .bind(this);
 
-        this.props.firebase.readStudentsOnce().then((value) => {
-          if (value) this.parseData(value)
-        })
+        this
+            .props
+            .firebase
+            .readStudentsOnce()
+            .then((value) => {
+                if (value) 
+                    this.parseData(value)
+            })
     }
 
     parseData = (data) => {
-      let parsed = []
+        let parsed = []
 
-      data.forEach((row) => {
-        parsed.push({name: row.FirstName, email: row.Email})
-      })
-      this.setState({data: parsed})
+        data.forEach((row) => {
+            let x = {
+              ...row, 
+              mill: row.mill, 
+              lathe: row.lathe, 
+              cncmill: row.cncmill,
+              cncrouter: row.cncrouter,
+              cncplasma: row.cncplasma
+            }
+            for (var att in x) {
+              if (x[att] === undefined) {
+                x[att] = false
+              }
+            }
+            parsed.push(x)
+        })
+
+        this.setState({data: parsed})
     }
 
     togglePopup()
     {
-        this.setState(
-            {
-                showPopup: !this.state.showPopup
-            }
-        );
+        this.setState({
+            showPopup: !this.state.showPopup
+        });
     }
 
+    handleCheck = (row, machine) => {
+      let fbID = this.state.data.indexOf(row)+1
+      let updatedUser = { ...row }
+      
+ 
+      updatedUser[machine] = !updatedUser[machine]
+      console.log(fbID)
+      console.log(updatedUser)
+      let newData = this.state.data.slice()
+      newData[fbID-1] = updatedUser
+      this.setState({data: newData})
+      this.props.firebase.updateUser(updatedUser, fbID)
+    }
+
+    cellFormatter1 = (cell, row, rowIndex) => {
+        return (<input type="checkbox" checked={this.state.data[rowIndex].mill} onClick={() => this.handleCheck(row, "mill")}/>);
+    }
+
+    
+
+    cellFormatter2 = (cell, row, rowIndex) => {
+      return (<input type="checkbox" checked={this.state.data[rowIndex].lathe} onClick={() => this.handleCheck(row, "lathe")}/>);
+    }
+
+    cellFormatter3 = (cell, row, rowIndex) => {
+      return (<input type="checkbox" checked={this.state.data[rowIndex].cncmill} onClick={() => this.handleCheck(row, "cncmill")}/>);
+    }
+
+    cellFormatter4 = (cell, row, rowIndex) => {
+      return (<input type="checkbox" checked={this.state.data[rowIndex].cncrouter} onClick={() => this.handleCheck(row, "cncrouter")}/>);
+    }
+
+    cellFormatter5 = (cell, row, rowIndex) => {
+      return (<input type="checkbox" checked={this.state.data[rowIndex].cncplasma} onClick={() => this.handleCheck(row, "cncplasma")}/>);
+    }
 
     render() {
-        return(
-            <ToolkitProvider className= "myTable"
-                bootstrap4 = {true}
+        return (
+            <ToolkitProvider
+                className="myTable"
+                bootstrap4={true}
                 keyField="Name"
-                data= {this.state.data}
-                
-                columns={ columns }
-                search
-                >
-                {
-                    props => (
-                    <div className = "problem">
-                        <div className = "options">
-                        <SearchBar { ...props.searchProps } />
-                        <button className= "hide">&nbsp;</button>
+                data={this.state.data}
+                columns={this.columns}
+                search>
+                {props => (
+                    <div className="problem">
+                        <div className="options">
+                            <SearchBar { ...props.searchProps }/>
+                            <button className="hide">&nbsp;</button>
                         </div>
 
-                        <BootstrapTable 
-                         bootstrap4 = {true}
-                        { ...props.baseProps }
-                        />
-                        <button onClick= {this.togglePopup.bind(this)}>Add Student</button>
-                        {this.state.showPopup ? 
-                            <AddPrompt 
-                                data = {this.state.data}
-                                closePopup={this.togglePopup.bind(this)}
-                            />
+                        <BootstrapTable bootstrap4={true} { ...props.baseProps }/>
+                        <button
+                            onClick={this
+                            .togglePopup
+                            .bind(this)}>Add Student</button>
+                        {this.state.showPopup
+                            ? <AddPrompt
+                                    data={this.state.data}
+                                    closePopup={this
+                                    .togglePopup
+                                    .bind(this)}/>
                             : null
-                        }
-                        <ExcelButton />
+}
+                        <ExcelButton/>
                     </div>
-                    )
-                }
+                )
+}
             </ToolkitProvider>
         )
     }
 }
 
-class AddPrompt extends NStudentTable {
+class AddPrompt extends NStudentTable {
     constructor(props) {
-      super(props);
-      this.state = 
-      {
-        data: this.props.data,
-        newName: '',
-        newContact: '',
-        //id: this.props.id,
-      };
-  
-      this.handleChangeUser = this.handleChangeUser.bind(this);
-      this.handleChangeContact = this.handleChangeContact.bind(this);
+        super(props);
+        this.state = {
+            data: this.props.data,
+            newName: '',
+            newContact: '',
+            //id: this.props.id,
+        };
 
-  
-      this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeUser = this
+            .handleChangeUser
+            .bind(this);
+        this.handleChangeContact = this
+            .handleChangeContact
+            .bind(this);
+
+        this.handleSubmit = this
+            .handleSubmit
+            .bind(this);
     }
-  
+
     handleChangeUser(event) {
-      this.setState({newName: event.target.value});
+        this.setState({newName: event.target.value});
     }
 
     handleChangeContact(event) {
         this.setState({newContact: event.target.value});
     }
-  
+
     handleSubmit(event) {
-      if (this.state.newName !== '')
-      {
-        var newArray = this.state.data.slice();   
-        newArray.push({ name: this.state.newName, email: this.state.newContact,});   
-        this.setState({data:newArray});
-        localStorage.setItem('data', JSON.stringify(newArray));
-        
-      }
-      else if (this.state.newName == '')
-      {
-        alert("Please enter a user");
-        event.preventDefault();
-      }
+        if (this.state.newName !== '') {
+            var newArray = this
+                .state
+                .data
+                .slice();
+            newArray.push({name: this.state.newName, email: this.state.newContact});
+            this.setState({data: newArray});
+            localStorage.setItem('data', JSON.stringify(newArray));
+
+        } else if (this.state.newName == '') {
+            alert("Please enter a user");
+            event.preventDefault();
+        }
     }
-  
+
     render() {
-      return (
-        <div className='Login'>
-          <div className='Login_inner'>
-            <div className='Close_bar'>
-              <button className ='closer' onClick={this.props.closePopup}>X</button>
+        return (
+            <div className='Login'>
+                <div className='Login_inner'>
+                    <div className='Close_bar'>
+                        <button className='closer' onClick={this.props.closePopup}>X</button>
+                    </div>
+
+                    <div className='SignIn'>
+                        Add a User
+                        <form className='SignForm' onSubmit={this.handleSubmit}>
+                            New User's Name:
+                            <label className='UserBar'>
+
+                                <input type="text" value={this.state.newName} onChange={this.handleChangeUser}/>
+                            </label>
+                            Contact Info:
+                            <label className='PasswordBar'>
+
+                                <input
+                                    type="text"
+                                    value={this.state.newContact}
+                                    onChange={this.handleChangeContact}/>
+                            </label>
+                            <input type="submit" value="Submit"/>
+                        </form>
+                    </div>
+
+                </div>
+
             </div>
-            
-            <div className ='SignIn'>
-            Add a User
-              <form className='SignForm' onSubmit={this.handleSubmit}>
-                New User's Name:
-                <label className='UserBar'>
-                  
-                  <input type="text" value={this.state.newName} onChange={this.handleChangeUser} />
-                </label>
-                Contact Info:
-                <label className='PasswordBar'>
-                    
-                    <input type="text" value={this.state.newContact} onChange={this.handleChangeContact} />
-                </label>
-                <input type="submit" value="Submit" />
-              </form>
-            </div>
-  
-          </div>
-   
-        </div>
-      );
+        );
     }
-  }
+}
 
+const StudentTable = compose(withRouter, withFirebase,)(NStudentTable);
 
-const StudentTable = compose(
-    withRouter,
-    withFirebase,
-  )(NStudentTable);
-
-export { StudentTable };
-
+export {StudentTable};
